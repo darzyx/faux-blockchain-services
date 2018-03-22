bd93fa25e0165175da7f35efb38fc2906aa4ca60
Jose Dario Sanchez <josedario.xyz@gmail.com>
2018-03-22 12:08:38 -0700

    Initial commit—uploads React app bolierplate to GitHub

---
+++ README.md
@@ -0,0 +1,7 @@
+# Faux Blockchain Services
+
+A website design/mockup for an imaginary blockchain services company.
+
+This is for demonstration purposes only.
+
+Copyright © 2018 Jose Dario Sanchez. All rights reserved.
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import registerServiceWorker from './utilities/registerServiceWorker'
+import './styles/index.css'
+import App from './App'
+
+const rootDOMNode = document.getElementById('root')
+
+ReactDOM.render(<App />, rootDOMNode)
+
+registerServiceWorker()
