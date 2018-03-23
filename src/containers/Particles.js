8212af51dfd4e4135544a98fa55d5bdea792cdae
Jose Dario Sanchez <josedario.xyz@gmail.com>
2018-03-23 13:54:30 -0700

    Creates array of points with coordinates

--- src/containers/Particles.js
+++ src/containers/Particles.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react'
 
 export default class Particles extends Component {
+  constructor(props) {
+    super(props)
+
+    this._initAnimation = this._initAnimation.bind(this)
+  }
+
+  componentDidMount() {
+    this._initAnimation()
+  }
+
+  _initAnimation() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+
+    const canvas = document.getElementById('particles')
+    canvas.width = width
+    canvas.height = height
+
+    const points = [ ]
+    const nPoints = Math.round(width * height / 20000)
+    for (let i = 0; i < nPoints; i++) {
+      const xPos = Math.round(Math.random() * width)
+      const yPos = Math.round(Math.random() * height)
+      const newPoint = { x: xPos, xOrigin: xPos, y: yPos, yOrigin: yPos }
+      points.push(newPoint)
+    }
+
+    console.log(points)
+  }
+
   render() {
     return (
       <canvas id='particles'></canvas>
