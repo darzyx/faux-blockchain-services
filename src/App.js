e6dd3633f62fdd770cc6d69efaeec67d45223ec7
Jose Dario Sanchez <josedario.xyz@gmail.com>
2018-03-23 13:36:50 -0700

    Adds gsap dependency for animations, creates Particles component to be animated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+import Particles from './containers/Particles'
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
+      <div id='app'>
+        <Particles />
       </div>
     )
   }
---
+++ src/containers/Particles.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Particles extends Component {
+  render() {
+    return (
+      <canvas id='particles'></canvas>
+    )
+  }
+}
